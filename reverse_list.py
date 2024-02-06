d29bf995debd138f5c42e4ff97aaa4914e1c40b9
Sircule14 <victor.jromar@gmail.com>
2024-02-06 09:31:33 -0700

    adds reverse list solution

---
+++ reverse_list.py
@@ -0,0 +1,16 @@
+
+#Revert list without pre build method.
+
+def reverse_list(list): #create a function that receives the list 
+    start = 0 # initialize variables two contain the start and the end of the list
+    end = len(list) -1
+    while start < end: #while to change the values on the list
+        list[start], list[end], = list[end], list[start] # the elements will change of position (start and end)
+
+        start += 1 #adds one to the start element 
+        end -= 1 #substract one to the end element
+    
+    return list #returnt the list 
+
+input_list = [1,2,3,4,5]
+print(reverse_list(input_list))
