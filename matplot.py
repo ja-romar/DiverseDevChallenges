13a91e6a067d26653cec697735fa05dad1b82d89
Sircule14 <victor.jromar@gmail.com>
2024-02-07 22:06:13 -0700

    ADD matplotlib example

---
+++ matplot.py
@@ -0,0 +1,11 @@
+#Example of visualization on matplotlib.
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+X = np.linspace(0, 10*np.pi, 1000)
+Y = np.sin(X)
+
+fig, ax = plt.subplots()
+ax .plot(X, Y)
+plt.show()
